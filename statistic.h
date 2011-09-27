c0ac30eb0627546dd56fdd81b3c0f17afd4b3b4f
Sidney Wang <sidneyttw@gmail.com>
2011-09-27 10:05:19 +0800

    Added comments fo statistic.h.
    Signed-off-by: SidneyTTW <sidneyttw@gmail.com>

--- statistic.h
+++ statistic.h
@@ -3,10 +3,21 @@
 
 #include <QObject>
 
+/**
+ * @brief A class to deal with the statistics of the game.
+ */
 class Statistic : public QObject
 {
   Q_OBJECT
 public:
+  /**
+   * @brief Type of the statistics.
+   *
+   * @warning Other infomation isn't stored currently, if you wish to add one
+   * or more things, you need to delete the file maintained by the program, and
+   * let the program create the file. Otherwise, the size of the file may be
+   * and the program may not run correctly.
+   */
   enum StatisticType {FlameGetCount=0,
                       StarGetCount,
                       FlameUsedCount,
@@ -22,10 +33,21 @@ public:
                       RotatePuzzleFinished,
                       RotatePuzzleTotal};
 
+  /**
+   * @brief Get a statistic.
+   */
   int getStatistic(StatisticType type);
+
+  /**
+   * @brief Change a statistic.
+   */
   void changeStatistic(StatisticType type, int value, bool add = false);
 
 signals:
+  /**
+   * @brief A signal to tell other part of the game that the statistic was
+   * changed
+   */
   void statisticChanged(Statistic::StatisticType type,
                         int lastValue,
                         int currentValue);
